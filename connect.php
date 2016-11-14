9932f9f25971a258aa348c5054ebb0dfbf7226a6
jacobo593 <jacobo593@gmail.com>
2016-11-14 09:31:43 -0500

    New code to run site with php

--- connect.php
+++ connect.php
@@ -5,3 +5,11 @@ $conn = mysqli_connect('localhost', 'root', '', 'test');
 if(!$conn) {
 	die("Connection Failed: " . mysqli_connect_error());
 }
+else
+	echo("Success");
+
+$sql = "SELECT * FROM resources";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+    	    	 	
+echo $row['name'];
--- test.php
+++ test.php
@@ -166,12 +166,14 @@
     <div class="container">
     	<div class="row">
     	    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+				<h2 class="section-heading">Fourth Type:</h2>
     	    	 <?php
-    	    	 	$sql = "SELECT * FROM resources";
+    	    	 	$sql = "SELECT name FROM resources";
     	    	 	$result = mysqli_query($conn, $sql);
-    	    	 	$row = mysqli_fetch_assoc($result);
-    	    	 	
-    	    	 	echo $row['name'];    	    	 
+					echo "<select classe='form-control'>";
+    	    	 	while($row = mysqli_fetch_array($result)){
+						echo "<option>" . $row['name'] . "</option>";
+					};    	 
     	    	 ?>
 			</div>
 		</div>
