973ac84d58fb53dcc037f4c5413b64f773357512
CharlesHerzog <charlesherzog@ymail.com>
2017-03-06 11:37:27 -0500

    Update firebase-data-dashboard.js

--- js/firebase-data-dashboard.js
+++ js/firebase-data-dashboard.js
@@ -1,9 +1,5 @@
 window.onload = function(){
-	// Initialize Firebase
-				
-				//Sets the content of question 1 to match firebase
-				var labor_force_size = document.getElementById('labor_force_size')
-				var labor_force_size_ref = firebase.database().ref('Data_Dashboard').child('labor_force_size');
-				labor_force_size_ref_ref.on('value', snap => labor_force_size.innerText = snap.val());
-        
-        }
+		var labor_force_size = document.getElementById('labor_force_size')
+		var labor_force_size_ref = firebase.database().ref('Data_Dashboard').child('labor_force_size');
+		labor_force_size_ref.on('value', snap => labor_force_size.innerText = snap.val());
+        };
