b1bd968283b1c12d4a95dd423e58c45f8ab9ba8a
lobaron89 <tnuckoll@umflint.edu>
2017-03-19 21:04:34 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -29,7 +29,8 @@ var rootref = firebase.database().ref();
 	var service = document.getElementById("service");
 	var phase = document.getElementById("phase");
 	var addBtn = document.getElementById("addBtn");
-
+	var address = document.getElementById("address");
+	var phone = document.getElementById("phone");
 
 
 function addResource () {
@@ -42,8 +43,9 @@ function addResource () {
       notes: ""+ notes.value +"",
       url: ""+ url.value +"",
       service: ""+ service.value +"",
-      phase: ""+ phase.value +""
-
+      phase: ""+ phase.value +"",
+      address: ""+ address.value +"",
+      phone: ""+ phone.value +""
     });
 	}
 
