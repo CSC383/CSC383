e2391c0c04006a0b550ee05a265569b28fa3bc61
CharlesHerzog <charlesherzog@ymail.com>
2017-03-22 15:11:40 -0400

    Update firebase-data-dashboard.js

--- js/firebase-data-dashboard.js
+++ js/firebase-data-dashboard.js
@@ -62,4 +62,42 @@ window.onload = function(){
 		var unemployment_rate = document.getElementById('unemployment_rate')
 		var unemployment_rate_ref = firebase.database().ref('data_dashboard').child('unemployment_rate');
 		unemployment_rate_ref.on('value', snap => unemployment_rate.innerText = snap.val());
+	
+		var ctx = document.getElementById("myChart");
+		var myChart = new Chart(ctx, {
+		    type: 'bar',
+		    data: {
+			labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+			datasets: [{
+			    label: '# of Votes',
+			    data: [3, 3, labor_force_size_ref, 3, 3],
+			    backgroundColor: [
+				'rgba(255, 99, 132, 0.2)',
+				'rgba(54, 162, 235, 0.2)',
+				'rgba(255, 206, 86, 0.2)',
+				'rgba(75, 192, 192, 0.2)',
+				'rgba(153, 102, 255, 0.2)',
+				'rgba(255, 159, 64, 0.2)'
+			    ],
+			    borderColor: [
+				'rgba(255,99,132,1)',
+				'rgba(54, 162, 235, 1)',
+				'rgba(255, 206, 86, 1)',
+				'rgba(75, 192, 192, 1)',
+				'rgba(153, 102, 255, 1)',
+				'rgba(255, 159, 64, 1)'
+			    ],
+			    borderWidth: 1
+			}]
+		    },
+		    options: {
+			scales: {
+			    yAxes: [{
+				ticks: {
+				    beginAtZero:true
+				}
+			    }]
+			}
+		    }
+		});
         };
