113f2ff04671c6e3f22bec6067a9ea27f03e3816
CharlesHerzog <charlesherzog@ymail.com>
2017-03-27 22:28:01 -0400

    Update firebase-data-dashboard.js

--- js/firebase-data-dashboard.js
+++ js/firebase-data-dashboard.js
@@ -174,79 +174,37 @@ window.onload = function(){
     		}
 	});
 	
-	var ftx = document.getElementById("myChart4");
-		var myChart = new Chart(ftx, {
-		    type: 'scatter',
-		    data: {
-			labels: ["Two", "Unemployment", "Average Commute", "Labor Force", "Broadband", "Churn Rate"],
-			datasets: [{
-			    label: 'Test Scatter Chart',
-			    data: [2, chart_variable, average_annual_commute_time_ref, labor_force_size_ref, broadband_access_ref, churn_rate_ref],
-				backgroundColor: [
-				'rgba(255, 99, 132, 0.2)',
-				'rgba(54, 162, 235, 0.2)',
-				'rgba(255, 206, 86, 0.2)',
-				'rgba(75, 192, 192, 0.2)',
-				'rgba(153, 102, 255, 0.2)',
-				'rgba(255, 159, 64, 0.2)'
-			    ],
-			    borderColor: [
-				'rgba(255,99,132,1)',
-				'rgba(54, 162, 235, 1)',
-				'rgba(255, 206, 86, 1)',
-				'rgba(75, 192, 192, 1)',
-				'rgba(153, 102, 255, 1)',
-				'rgba(255, 159, 64, 1)'
-			    ],
-			    borderWidth: 1
-			}]
-		    },
-		    options: {
-			scales: {
-			    yAxes: [{
-				ticks: {
-				    beginAtZero:true
+	var bubble_data = {
+		datasets: [{
+			label: 'First Dataset',
+			data: [
+				{
+					x: 20,
+					y: 30,
+					r: 15
+				},
+				{
+					x: 40,
+					y: 10,
+					r: 10
 				}
-			    }]
-			}
-		    }
-		});
+			],
+			backgroundColor:"#FF6384",
+			hoverBackgroundColor: "#FF6384",
+		}]
+	};
 	
-	var gtx = document.getElementById("myChart5");
-		var myChart = new Chart(gtx, {
-		    type: 'bubble',
-		    data: {
-			labels: ["Two", "Unemployment", "Average Commute", "Labor Force", "Broadband", "Churn Rate"],
-			datasets: [{
-			    label: 'Test Bubble Graph',
-			    data: [2, chart_variable, average_annual_commute_time_ref, parseInt(labor_force_size.innerText), broadband_access_ref, churn_rate_ref],
-				backgroundColor: [
-				'rgba(255, 99, 132, 0.2)',
-				'rgba(54, 162, 235, 0.2)',
-				'rgba(255, 206, 86, 0.2)',
-				'rgba(75, 192, 192, 0.2)',
-				'rgba(153, 102, 255, 0.2)',
-				'rgba(255, 159, 64, 0.2)'
-			    ],
-			    borderColor: [
-				'rgba(255,99,132,1)',
-				'rgba(54, 162, 235, 1)',
-				'rgba(255, 206, 86, 1)',
-				'rgba(75, 192, 192, 1)',
-				'rgba(153, 102, 255, 1)',
-				'rgba(255, 159, 64, 1)'
-			    ],
-			    borderWidth: 1
-			}]
-		    },
-		    options: {
-			scales: {
-			    yAxes: [{
-				ticks: {
-				    beginAtZero:true
-				}
-			    }]
-			}
-		    }
-		});
-        };
+	var ftx = document.getElementById("myChart4");
+	var myBubbleChart = new Chart(ftx,{
+    		type: 'bubble',
+    		data: bubble_data,
+    		options: {
+        		elements: {
+           			 points: {
+                			borderWidth: 1,
+                			borderColor: 'rgb(0, 0, 0)'
+            			}
+        		}
+    		}
+	});
+};
