76b1dc29ad9f2bceb1e4ca607efe7a7080b72161
aaron-rutledge <aarutled@umflint.edu>
2017-04-10 18:50:20 -0400

    Added makeModal(input)

--- js/firebase-resources.js
+++ js/firebase-resources.js
@@ -1,6 +1,15 @@
 //This page handles all resources page content
 
-
+//defines and appends the submit button for the resource modal
+function makeModal(input)
+{
+  rootref.child("resources").orderByChild("name").equalTo(input).on("child_added", function(snap)
+  {
+    var nameOf = snap.child("name").val();
+    
+    $("#modalSubmit").append("<input onclick='makeReview(\""+ input +"\")' type='submit' class='btn btn-success btn-send' value='Submit'")
+  }
+}
 
 
 //Loads all resources on page load
