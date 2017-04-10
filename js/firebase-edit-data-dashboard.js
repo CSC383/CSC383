043b50867a7ea70e656173c8d75a9cbfb652221b
CharlesHerzog <charlesherzog@ymail.com>
2017-04-10 14:34:06 -0400

    Update firebase-edit-data-dashboard.js

--- js/firebase-edit-data-dashboard.js
+++ js/firebase-edit-data-dashboard.js
@@ -44,45 +44,12 @@ var resourceRef = firebase.database();
 
 function updateStatistics(statisticID) {
 	var id = statisticID;
-	var service = id + "service";
-	var phase = id + "phase";
-	var county = id + "county";
-	var name = id + "name";
-	var address = id + "address";
-	var phone = id + "phone";
-	var lat = id + "lat";
-	var lng = id + "lng";
-	var contact = id + "contact";
-	var restriction = id + "restriction";
-	var notes = id + "notes";
-	var url = id + "url";
+	var statValue = id + "Value";
 
-	var serviceUpdate = document.getElementById(service);
-	var phaseUpdate = document.getElementById(phase);
-	var countyUpdate = document.getElementById(county);
-	var nameUpdate = document.getElementById(name);
-	var addressUpdate = document.getElementById(address);
-	var phoneUpdate = document.getElementById(phone);
-	var latUpdate = document.getElementById(lat);
-	var lngUpdate = document.getElementById(lng);
-	var contactUpdate = document.getElementById(contact);
-	var restrictionUpdate = document.getElementById(restriction);
-	var notesUpdate = document.getElementById(notes);
-	var urlUpdate = document.getElementById(url);
+	var valueUpdate = document.getElementById(statValue);
 
-	resourceRef.ref("Resources").child(id).update({
-		service: ""+ serviceUpdate.value +"",
-		phase: ""+ phaseUpdate.value +"",
-		county: ""+ countyUpdate.value +"",
-		name: ""+ nameUpdate.value +"",
-		address: ""+ addressUpdate.value +"",
-		phone: ""+ phoneUpdate.value +"",
-		contact: ""+ contactUpdate.value + "",
-		restriction: ""+ restrictionUpdate.value +"",
-		notes: ""+ notesUpdate.value +"",
-		url: ""+ urlUpdate.value +"",
-		lat: ""+ latUpdate.value +"",
-		lng: ""+ lngUpdate.value +""
+	resourceRef.ref("data_dashboard").child(id).update({
+		statisticID: ""+ valueUpdate.value +"",
 	});
 };
 
@@ -112,7 +79,7 @@ function allStatistics() {
 		var statValue = snap.val();
 
 		//Creates table with resources pulled from firebase
-		$("#table_body").append("<tr><td><input id='"+id+"Statistic' type='text' value=\""+stringID+"\" ></input></td><td><input id='"+id+"Value' type='text'  value=\""+statValue+"\" ></input></td><td><input type='submit' class='btn btn-success btn-send' value='Delete' onclick='deleteResource(\""+ stringID + "\")'></input></td><td><input type='submit' class='btn btn-success btn-send' value='Update' onclick='updateResource(\""+ stringID + "\")''></input></td></tr>"
+		$("#table_body").append("<tr><td><h3 id='"+id+"Statistic' type='text' value=\""+stringID+"\" ></h3></td><td><input id='"+id+"Value' type='text'  value=\""+statValue+"\" ></input></td><td><input type='submit' class='btn btn-success btn-send' value='Delete' onclick='deleteResource(\""+ stringID + "\")'></input></td><td><input type='submit' class='btn btn-success btn-send' value='Update' onclick='updateResource(\""+ stringID + "\")''></input></td></tr>"
 		);
 		});
 };
