5a823e12dee98e0d0ea0d81942d6fd296a806bd0
CharlesHerzog <charlesherzog@ymail.com>
2017-04-10 11:50:59 -0400

    Update firebase-data-dashboard.js

--- js/firebase-data-dashboard.js
+++ js/firebase-data-dashboard.js
@@ -1,58 +1,5 @@
 window.onload = function(){
-	
-		var average_annual_commute_time = document.getElementById('average_annual_commute_time')
-		var average_annual_commute_time_ref = firebase.database().ref('data_dashboard').child('average_annual_commute_time');
-		average_annual_commute_time_ref.on('value', snap => average_annual_commute_time.innerText = snap.val());
-	
-		var broadband_access = document.getElementById('broadband_access')
-		var broadband_access_ref = firebase.database().ref('data_dashboard').child('broadband_access');
-		broadband_access_ref.on('value', snap => broadband_access.innerText = snap.val());
-	
-		var churn_rate = document.getElementById('churn_rate')
-		var churn_rate_ref = firebase.database().ref('data_dashboard').child('churn_rate');
-		churn_rate_ref.on('value', snap => churn_rate.innerText = snap.val());
-	
-		var closings = document.getElementById('closings')
-		var closings_ref = firebase.database().ref('data_dashboard').child('closings');
-		closings_ref.on('value', snap => closings.innerText = snap.val());
-	
-		var cost_of_living = document.getElementById('cost_of_living')
-		var cost_of_living_ref = firebase.database().ref('data_dashboard').child('cost_of_living');
-		cost_of_living_ref.on('value', snap => cost_of_living.innerText = snap.val());
-	
-		var jobs_employed_resident_ratio = document.getElementById('jobs_employed_resident_ratio')
-		var jobs_employed_resident_ratio_ref = firebase.database().ref('data_dashboard').child('jobs_employed_resident_ratio');
-		jobs_employed_resident_ratio_ref.on('value', snap => jobs_employed_resident_ratio.innerText = snap.val());
-	
-		var new_business_starts = document.getElementById('new_business_starts')
-		var new_business_starts_ref = firebase.database().ref('data_dashboard').child('new_business_starts');
-		new_business_starts_ref.on('value', snap => new_business_starts.innerText = snap.val());
-	
-		var new_housing_costs = document.getElementById('new_housing_costs')
-		var new_housing_costs_ref = firebase.database().ref('data_dashboard').child('new_housing_costs');
-		new_housing_costs_ref.on('value', snap => new_housing_costs.innerText = snap.val());
-	
-		var poverty_rate = document.getElementById('poverty_rate')
-		var poverty_rate_ref = firebase.database().ref('data_dashboard').child('poverty_rate');
-		poverty_rate_ref.on('value', snap => poverty_rate.innerText = snap.val());
-	
-		var sbir_grants = document.getElementById('sbir_grants')
-		var sbir_grants_ref = firebase.database().ref('data_dashboard').child('sbir_grants');
-		sbir_grants_ref.on('value', snap => sbir_grants.innerText = snap.val());
-	
-		var stage_ii_establishments = document.getElementById('stage_ii_establishments')
-		var stage_ii_establishments_ref = firebase.database().ref('data_dashboard').child('stage_ii_establishments');
-		stage_ii_establishments_ref.on('value', snap => stage_ii_establishments.innerText = snap.val());
-	
-		var stem_employment = document.getElementById('stem_employment')
-		var stem_employment_ref = firebase.database().ref('data_dashboard').child('stem_employment');
-		stem_employment_ref.on('value', snap => stem_employment.innerText = snap.val());
-	
-		var unemployment_rate = document.getElementById('unemployment_rate')
-		var unemployment_rate_ref = firebase.database().ref('data_dashboard').child('unemployment_rate');
-		unemployment_rate_ref.on('value', snap => unemployment_rate.innerText = snap.val());
-	
-	
+
 		firebase.database().ref("data_dashboard").on("value", snap => {	
 			
 			var labor_force_size_value = snap.child("labor_force_size").val();
@@ -103,11 +50,17 @@ window.onload = function(){
 					}
 		    		}
 			});
-		    
 					
 			var unemployment_rate_value = snap.child("unemployment_rate").val();
 			var poverty_rate_value = snap.child("poverty_rate").val();
 			var closings_value = snap.child("closings").val();
+			
+			var unemployment_rate_element = document.getElementById('unemployment_rate')
+			unemployment_rate_element.innerText = unemployment_rate_value
+			var poverty_rate_element = document.getElementById('poverty_rate')
+			poverty_rate_element.innerText = poverty_rate_value
+			var closings_element = document.getElementById('closings')
+			closings_element.innerText = closings_value
 	
 			var dtx = document.getElementById("myChart2");
 			var myChart = new Chart(dtx, {
@@ -151,6 +104,15 @@ window.onload = function(){
 			var new_housing_costs_value = snap.child("new_housing_costs").val();
 			var average_annual_commute_time_value = snap.child("average_annual_commute_time").val();
 			var jobs_employed_resident_ratio_value = snap.child("jobs_employed_resident_ratio").val();
+			
+			var cost_of_living_element = document.getElementById('cost_of_living')
+			cost_of_living_element.innerText = cost_of_living_value
+			var new_housing_costs_element = document.getElementById('new_housing_costs')
+			new_housing_costs_element.innerText = new_housing_costs_value
+			var average_annual_commute_time_element = document.getElementById('average_annual_commute_time')
+			average_annual_commute_time_element.innerText = average_annual_commute_time_value
+			var jobs_employed_resident_ratio_element = document.getElementById('jobs_employed_resident_ratio')
+			jobs_employed_resident_ratio_element.innerText = jobs_employed_resident_ratio_value
 	
 			var pie_data = {
     				labels: [
@@ -191,6 +153,19 @@ window.onload = function(){
 			var stem_employment_value = snap.child("stem_employment").val();
 			var broadband_access_value = snap.child("broadband_access").val();
 			var churn_rate_value = snap.child("churn_rate").val();
+			
+			var new_business_starts_element = document.getElementById('new_business_starts')
+			new_business_starts_element.innerText = new_business_starts_value
+			var stage_ii_establishments_element = document.getElementById('stage_ii_establishments')
+			stage_ii_establishments_element.innerText = stage_ii_establishments_value
+			var sbir_grants_element = document.getElementById('sbir_grants')
+			sbir_grants_element.innerText = sbir_grants_value
+			var stem_employment_element = document.getElementById('stem_employment')
+			stem_employment_element.innerText = stem_employment_value
+			var broadband_access_element = document.getElementById('broadband_access')
+			broadband_access_element.innerText = broadband_access_value
+			var churn_rate_element = document.getElementById('churn_rate')
+			churn_rate_element.innerText = churn_rate_value
 	
 			var bubble_data = {
 				datasets: [{
