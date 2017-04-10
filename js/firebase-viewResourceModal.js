16e512c1ef1d0f0eb9cb4c3e7137c4e121423f3f
aaron-rutledge <aarutled@umflint.edu>
2017-04-10 18:17:33 -0400

    Create firebase-viewResourceModal.js

---
+++ js/firebase-viewResourceModal.js
@@ -0,0 +1,57 @@
+function getCurrent(key)
+{
+  var resourceRef = rootref.child("Resources");
+
+
+  resourceRef.orderByChild("name").equalTo(input).on("child_added", function(snap)
+  {
+    snap.child("reviews").forEach(function(shot)
+    {     
+      var user = shot.child("user").val();
+      var rating = shot.child("rating").val();
+      var rBody = shot.child("text").val();
+
+      $("#modal_body").append("<tr><td>" + user + "</td><td>" + rating + "</td></tr><tr><td>" + rBody+ "</td></tr>");
+    });
+  },
+  function(error)
+  {
+    $("#modal_body").append("<tr><td>Sorry! No one has reviewed this content! Be the first!</td></tr>")
+  });
+}
+
+function makeReivew(input)
+{
+  var rBody = document.getElementById("rMaker");
+  var score = document.getElementById("rater");
+  var userID = document.getElementById("username");
+
+  rootref.child("Resources").orderByChild("name").equalTo(input).on("child_added", function(snap)
+  {
+    var pushRef = snap.child("reviews").push().set(
+      {
+      user: "" + userID.value + "",
+      rating: "" + score.value + "",
+      text: "" + rBody.value + ""
+      });
+  }
+}
+
+function getAggregateRating(input)
+{
+  var i = 0;
+  var j = 0;
+  rootref.child("Resources").orderByChild("name").equalTo(input).once("child_added", function(snap)
+  {
+    snap.child("reviews").forEach(function(shot)
+    {
+      j = j + shot.child("rating").val();
+      i++;
+      
+    }
+  }
+
+  var rating = j/i;
+  
+
+}
