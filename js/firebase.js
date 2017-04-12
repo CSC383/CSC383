1d70a2151f9a7add75d8628c0663c5fd7be92202
lobaron89 <tnuckoll@umflint.edu>
2017-04-12 14:14:50 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -36,15 +36,13 @@ var rootref = firebase.database().ref();
 var address = document.getElementById("address");
 function codeAddress(){
 var geocode;	
-function codeAddress() {
+
     var address = document.getElementById("address");
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == 'OK') {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
+        var latlng = results[0].geometry.location
+        
+        
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
       }
